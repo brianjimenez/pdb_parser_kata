1a885dfee7b936cee5215cde032968b4d912e252
Brian Jimenez <bjimenez@bsc.es>
2018-01-24 18:49:29 +0100

    Red: create atom

---
+++ test/test_atom.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+import unittest
+
+
+class TestAtom(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_atom(self):
+        atom = Atom()
+
+
+if __name__ == '__main__':
+    unittest.main()
